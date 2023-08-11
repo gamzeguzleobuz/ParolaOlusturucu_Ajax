74ffe3e274952b25f76541defd6e87e8acd2c289
gamzeguzleobuz <gmzguzle@gmail.com>
2023-08-11 12:57:09 +0300

    Add project files.

---
+++ ParolaOlusturucu/Controllers/ParolaUreticisiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ParolaOlusturucu.Controllers
+{
+    public class ParolaUreticisiController : Controller
+    {
+        static Random rnd = new Random();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Uret(int uzunluk)
+        {
+            string havuz = "abcdefghijklmnoprstuvyzqwxABCDEFGHIJKLMNOPRSTUVYZQWX0123456789!@#$%^&*";
+            string sonuc = "";
+            for (int i = 0; i < uzunluk; i++)
+            {
+                sonuc += havuz[rnd.Next(havuz.Length)];
+            }
+            return Content(sonuc);
+        }
+    }
+}
